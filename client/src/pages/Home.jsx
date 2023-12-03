054f101c306542ecb37c793b297b1ecfcb38f261
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-12-03 11:53:15 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8000/api/v1/post', {
+        const response = await fetch('https://encouraging-ruby-underclothes.cyclic.app/api/v1/post', {
           method: 'GET',
           headers: {
             'content-type': 'application/json',
